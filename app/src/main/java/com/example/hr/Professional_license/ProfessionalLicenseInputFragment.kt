35a9cd9f175af9cdfe707eb680cdcb202890bf75
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-03-12 22:51:20 +0700

    update view recy

---
+++ app/src/main/java/com/example/hr/Professional_license/ProfessionalLicenseInputFragment.kt
@@ -0,0 +1,26 @@
+package com.example.hr.Professional_license
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.hr.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ProfessionalLicenseInputFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_professional_license_input, container, false)
+    }
+
+
+}
