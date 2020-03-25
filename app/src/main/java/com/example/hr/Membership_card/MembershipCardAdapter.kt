1172f5a684883263ae47d6867ea8d02ccfbd04ec
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-03-25 15:53:07 +0700

    Update Membership Card

---
+++ app/src/main/java/com/example/hr/Membership_card/MembershipCardAdapter.kt
@@ -0,0 +1,87 @@
+package com.example.hr.Membership_card
+
+import com.example.hr.R
+
+package com.example.projectmymoney.adapter
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.RecyclerView
+
+import org.json.JSONArray
+
+class MembershipCardAdapter (fragmentActivity: FragmentActivity, val dataSource: JSONArray, val account_username:String) : RecyclerView.Adapter<MembershipCardAdapter.Holder>() {
+
+    private val thiscontext : Context = fragmentActivity.baseContext
+    private val thisActivity = fragmentActivity
+
+    class Holder(view : View) : RecyclerView.ViewHolder(view) {
+        private val View = view
+        lateinit var layout : LinearLayout
+        lateinit var view_professional_name: TextView
+        lateinit var view_member_number: TextView
+        lateinit var view_issue_date: TextView
+        lateinit var view_expiration_date: TextView
+
+        fun Holder(){
+            layout = View.findViewById<View>(R.id.recy_membership_card_layout) as LinearLayout
+            view_professional_name = View.findViewById<View>(R.id.view_professional_name) as TextView
+            view_member_number = View.findViewById<View>(R.id.view_member_number) as TextView
+            view_issue_date = View.findViewById<View>(R.id.view_issue_date) as TextView
+            view_expiration_date = View.findViewById<View>(R.id.view_expiration_date) as TextView
+        }
+    }
+
+    override fun onCreateViewHolder(parent : ViewGroup, viewType: Int): Holder {
+        return Holder(LayoutInflater.from(parent.context).inflate(R.layout.recy_membership_card, parent, false))
+    }
+
+
+    override fun getItemCount(): Int {
+        return dataSource.length()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+
+        holder.Holder()
+
+        holder.view_professional_name.setText( dataSource.getJSONObject(position).getString("categories_name").toString() )
+        holder.view_member_number.setText( dataSource.getJSONObject(position).getString("transaction_note").toString() )
+        holder.view_issue_date.setText( dataSource.getJSONObject(position).getString("transaction_date").toString() )
+        holder.view_expiration_date.setText( dataSource.getJSONObject(position).getString("transaction_date").toString() )
+
+
+
+        holder.layout.setOnClickListener {
+            var key = dataSource.getJSONObject(position).getString("key").toString()
+            var username = dataSource.getJSONObject(position).getString("username").toString()
+            var professional_name = dataSource.getJSONObject(position).getString("categories_name").toString()
+            var member_number = dataSource.getJSONObject(position).getString("categories_type").toString()
+            var issue_date = dataSource.getJSONObject(position).getString("transaction_amount").toString()
+            var expiration_date = dataSource.getJSONObject(position).getString("transaction_date").toString()
+
+
+            val fm = thisActivity.supportFragmentManager
+            val transaction: FragmentTransaction = fm!!.beginTransaction()
+            val Fragment = MembershipCardInputFragment().newInstance(key, username, professional_name, member_number, issue_date, expiration_date)
+            transaction.replace(R.id.contentContainer, Fragment,"MembershipCardInputFragment")
+            transaction.addToBackStack("MembershipCardInputFragment")
+            transaction.commit()
+        }
+
+    }
+
+
+
+
+
+}
+
--- app/src/main/java/com/example/hr/Membership_card/MembershipCardInputFragment.kt
+++ app/src/main/java/com/example/hr/Membership_card/MembershipCardInputFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 import com.example.hr.R
 
@@ -13,13 +15,57 @@ import com.example.hr.R
  * A simple [Fragment] subclass.
  */
 class MembershipCardInputFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var obj_hr_work_experience : hr_work_experience
+
+    var Str_key = "" // key obj_hr_work_experience from firebase
+
+    data class hr_work_experience(
+        var username: String? = "",
+        var professional_name: String? = "",
+        var member_number: String? = "",
+        var issue_date: String? = "",
+        var expiration_date: String? = ""
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_membership_card_input, container, false)
+        val view =  inflater.inflate(R.layout.fragment_membership_card_input, container, false)
+
+        return view
+    }
+
+
+    fun newInstance(key: String, username:String, professional_name: String, member_number:String, issue_date:String, expiration_date:String): MembershipCardInputFragment {
+        val fragment = MembershipCardInputFragment()
+        val bundle = Bundle()
+        bundle.putString("key", key)
+        bundle.putString("username", username)
+        bundle.putString("professional_name", professional_name)
+        bundle.putString("member_number", member_number)
+        bundle.putString("issue_date", issue_date)
+        bundle.putString("expiration_date", expiration_date)
+        fragment.setArguments(bundle)
+        return fragment
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val bundle = arguments
+        if (bundle != null) {
+            obj_hr_work_experience = hr_work_experience(
+                bundle.getString("username").toString(),
+                bundle.getString("professional_name").toString(),
+                bundle.getString("member_number").toString(),
+                bundle.getString("issue_date").toString(),
+                bundle.getString("expiration_date").toString()
+            )
+            Str_key = bundle.getString("key").toString()
+        }
     }
 
 
