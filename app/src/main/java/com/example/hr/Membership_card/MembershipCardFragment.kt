7de0285acd8925b89390a3f24dd9703b888e2434
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-03-25 21:16:36 +0700

    Update Adrress [complete]

--- app/src/main/java/com/example/hr/Membership_card/MembershipCardFragment.kt
+++ app/src/main/java/com/example/hr/Membership_card/MembershipCardFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hr.Address.AddressFragment
 import com.example.hr.Education.EducationInputFragment
 
 import com.example.hr.R
@@ -129,6 +130,22 @@ class MembershipCardFragment : Fragment() {
 
             return view
         }
+
+    fun newInstance(username:String): MembershipCardFragment {
+        val fragment = AddressFragment()
+        val bundle = Bundle()
+        bundle.putString("username", username)
+        fragment.setArguments(bundle)
+        return fragment
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val bundle = arguments
+        if (bundle != null) {
+            account_username = bundle.getString("username").toString()
+        }
+    }
 
 
 }
